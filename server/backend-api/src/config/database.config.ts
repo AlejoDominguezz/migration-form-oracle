16d2584474275768cc0b05af8ef44947eac145fc
AlejoDominguezz <alejomariodominguez@gmail.com>
2025-09-18 18:51:52 -0300

    feat(bd): add orm config and module database

---
+++ server/backend-api/src/config/database.config.ts
@@ -0,0 +1,13 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const databaseConfig = (): TypeOrmModuleOptions => ({
+  type: 'postgres',
+  host: 'localhost',
+  port: parseInt(process.env.POSTGRES_EXTERNAL_PORT),
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  entities: [__dirname + '/../entitites/*.entity{.ts,.js}'],
+  synchronize: false,
+  logging: process.env.NODE_ENV === 'development',
+});
