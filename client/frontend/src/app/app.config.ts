563db2bd8759d739c63c773fdf1029c18510fda4
AlejoDominguezz <alejomariodominguez@gmail.com>
2025-09-19 11:30:53 -0300

    feat(environment): add development environment configuration and HTTP client provider

--- client/frontend/src/app/app.config.ts
+++ client/frontend/src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
 
@@ -55,6 +56,7 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
+    provideHttpClient(),
     providePrimeNG({
       theme: {
         preset: MyAuraPreset,
