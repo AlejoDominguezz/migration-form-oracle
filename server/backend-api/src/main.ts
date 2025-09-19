06e8ca733aadb2c89517f9b0636afba80c9f1f22
AlejoDominguezz <alejomariodominguez@gmail.com>
2025-09-19 11:33:39 -0300

    feat(cors): enable CORS with dynamic origin and method configuration

--- server/backend-api/src/main.ts
+++ server/backend-api/src/main.ts
@@ -4,6 +4,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.enableCors({
+    origin: process.env.FRONT_URL,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+  
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
